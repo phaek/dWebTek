59793919d281b5887d08be2a7a61cd6025405014
phaek <drasmus@gmail.com>
2017-02-04 14:14:01 +0100

    Nu med dynamisk mængde produkter pr <tr> baseret på width af #produktTabel (kun aktiveret ved refreshing - indtil videre)

--- JS/product.js
+++ JS/product.js
@@ -1,14 +1,25 @@
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.open("GET", "products.xml", false);
+xmlhttp.send();
+var xmlDoc = xmlhttp.responseXML;
+
 function onLoad() {
 
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.open("GET", "products.xml", false);
-    xmlhttp.send();
-    var xmlDoc = xmlhttp.responseXML;
+    if ($('#produktTabel').css('width').substring(0, $('#produktTabel').css('width').length-2) > 1150) {
+        $('#produktTabel').find('td').css('width', '33%');
+        loadProducts(3);
+    }
 
-    for (var i = 0; i < xmlDoc.getElementsByTagName('products')[0].childNodes.length; i++) {
-        if (i % 2 === 0)
-            $('#produktTabel').append('<tr>', null);
+    else {
+        loadProducts(2);
+    }
 
-        $('#produktTabel tr:last').append('<td><a href="#"><p class="prodNavn"><img src="' + xmlDoc.getElementsByTagName("img")[i].childNodes[0].nodeValue + '"> ' + xmlDoc.getElementsByTagName("prodNavn")[i].childNodes[0].nodeValue + '</p></a><p class="prodPris">' + xmlDoc.getElementsByTagName("pris")[i].childNodes[0].nodeValue + 'kr</p>');
+
+    function loadProducts(n) {
+        for (var i = 0; i < (xmlDoc.getElementsByTagName('products')[0].childNodes.length - 7); i++) {
+            if (i % n === 0)
+                $('#produktTabel').append('<tr>', null);
+            $('#produktTabel tr:last').append('<td><a href="#"><p class="prodNavn"><img src="' + xmlDoc.getElementsByTagName("img")[i].childNodes[0].nodeValue + '"> ' + xmlDoc.getElementsByTagName("prodNavn")[i].childNodes[0].nodeValue + '</p></a><p class="prodPris">' + xmlDoc.getElementsByTagName("pris")[i].childNodes[0].nodeValue + ' kr</p>');
+        }
     }
 }
