1ff01c3ada6bc4eda410630c24b01c1356c5ff5a
phaek <drasmus@gmail.com>
2017-02-12 09:46:13 +0100

    Smårettelser

--- JS/product.js
+++ JS/product.js
@@ -6,7 +6,9 @@ var xmlDoc = xmlhttp.responseXML;
 function onLoad() {
 
 
+    //noinspection JSUnresolvedFunction
     if ($('#produktTabel').css('width').substring(0, $('#produktTabel').css('width').length-2) > 1150) {
+        //noinspection JSUnresolvedFunction
         $('#produktTabel').find('td').css('width', '33%');
         loadProducts(3);
     }
@@ -17,10 +19,11 @@ function onLoad() {
 
 
     function loadProducts(n) {
-        for (var i = 0; i < (xmlDoc.getElementsByTagName('items')[0].childNodes.length)-(xmlDoc.getElementsByTagName('items')[0].childNodes.length/1.5); i++) {
+        for (var i = 0; i < (xmlDoc.getElementsByTagName('items')[0].childNodes.length)-(xmlDoc.getElementsByTagName('items')[0].childNodes.length/1.75); i++) {
             if (i % n === 0)
                 $('#produktTabel').append('<tr>', null);
-            $('#produktTabel tr:last').append('<td><a href="#"><p class="prodNavn"><img src="' + xmlDoc.getElementsByTagName("itemURL")[i].childNodes[0].nodeValue + '" alt=""> ' + xmlDoc.getElementsByTagName("itemName")[i].childNodes[0].nodeValue + '</p></a><p class="prodPris">' + xmlDoc.getElementsByTagName("itemPrice")[i].childNodes[0].nodeValue + ' kr</p>');
+            //noinspection JSUnresolvedFunction
+            $('#produktTabel').find('tr:last').append('<td><a href="#"><p class="prodNavn"><img src="' + xmlDoc.getElementsByTagName("itemURL")[i].childNodes[0].nodeValue + '" alt=""> ' + xmlDoc.getElementsByTagName("itemName")[i].childNodes[0].nodeValue + '</p></a><p class="prodPris">' + xmlDoc.getElementsByTagName("itemPrice")[i].childNodes[0].nodeValue + ' kr</p>', "");
         }
     }
 }
