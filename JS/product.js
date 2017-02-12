665701d957b5eef35ab17132cb2ffcda61c7c6e9
phaek <drasmus@gmail.com>
2017-02-12 09:37:10 +0100

    Algebraisk korrigering for færre fejlmeddelelser.

--- JS/product.js
+++ JS/product.js
@@ -17,7 +17,7 @@ function onLoad() {
 
 
     function loadProducts(n) {
-        for (var i = 0; i < (xmlDoc.getElementsByTagName('items')[0].childNodes.length); i++) {
+        for (var i = 0; i < (xmlDoc.getElementsByTagName('items')[0].childNodes.length)-(xmlDoc.getElementsByTagName('items')[0].childNodes.length/1.5); i++) {
             if (i % n === 0)
                 $('#produktTabel').append('<tr>', null);
             $('#produktTabel tr:last').append('<td><a href="#"><p class="prodNavn"><img src="' + xmlDoc.getElementsByTagName("itemURL")[i].childNodes[0].nodeValue + '" alt=""> ' + xmlDoc.getElementsByTagName("itemName")[i].childNodes[0].nodeValue + '</p></a><p class="prodPris">' + xmlDoc.getElementsByTagName("itemPrice")[i].childNodes[0].nodeValue + ' kr</p>');
