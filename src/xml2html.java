8bfaf7c11adbf8979842bcbb03b2983d8d223d5b
phaek <drasmus@gmail.com>
2017-02-21 22:41:08 +0100

    Omskrevet fra bunden. Nu helt uden JDOM og kun i pure Java, præcis som opgavebeskrivelsen lød på.

--- src/xml2html.java
+++ src/xml2html.java
@@ -1,11 +1,8 @@
-import org.jdom2.*;
-import org.jdom2.input.SAXBuilder;
-import org.jdom2.output.XMLOutputter;
-
+import javax.faces.bean.ManagedBean;
 import javax.faces.component.FacesComponent;
 import javax.faces.component.UIComponentBase;
 import javax.faces.context.FacesContext;
-import java.io.ByteArrayInputStream;
+import javax.faces.context.ResponseWriter;
 import java.io.IOException;
 
 
@@ -23,42 +20,30 @@ public class xml2html extends UIComponentBase {
 
     @Override
     public void encodeEnd(FacesContext context) throws IOException{
+        ResponseWriter out = context.getResponseWriter();
 
-        String value = (String) getAttributes().get("value");
-
-        try {
-            Document old = new SAXBuilder().build(new ByteArrayInputStream(value.getBytes()));
-            Element newRoot = (Element) reverseConversion(old.getRootElement());
-            Document newDoc = new Document(newRoot);
-            new XMLOutputter().output(newDoc, context.getResponseWriter());
-        } catch (JDOMException ignored) {}
-
+        out.startElement("div", null);
+        out.write(replacer((String) getAttributes().get("value")));
+        out.endElement("div");
     }
 
-    private Content reverseConversion(Content old) {
+    public String presenter(String in) {
 
-        if(old instanceof Text) return new Text(((Text)old).getText());
+        String out = in.replace("<w:document xmlns:w=\"http://www.cs.au.dk/dWebTek/2014\">", "");
+        out = out.replace("</w:document>", "");
+        out = out.replace("<w:document xmlns:w=\"http://www.cs.au.dk/dWebTek/2014\" />", "");
+        out = out.replaceAll("w:", "");
 
-        Element currEle = (Element) old;
-        Element htmlElement = null;
+        return out.trim();
+    }
 
-        if(currEle.getName().equals("italics") ){
-            htmlElement = new Element("i");
-        } else if (currEle.getName().equals("bold") ){
-            htmlElement = new Element("b");
-        } else if (currEle.getName().equals("list") ){
-            htmlElement = new Element("ul");
-        } else if (currEle.getName().equals("item") ){
-            htmlElement = new Element("li");
-        }else if (currEle.getName().equals("document") ){
-            htmlElement = new Element("div");
-        }
+    private String replacer(String in) {
+        String out = in.replaceAll("w:document", "div");
+        out = out.replaceAll("w:italics", "i");
+        out = out.replaceAll("w:bold", "b");
+        out = out.replaceAll("w:item", "li");
+        out = out.replaceAll("w:list", "ul");
 
-        for (Content e : currEle.getContent()){
-            assert htmlElement != null;
-            htmlElement.addContent(reverseConversion(e));
-
-        }
-        return htmlElement;
+        return out;
     }
 }
