5328dfa4afedf67c202211e6b84a3093cae06e42
phaek <drasmus@gmail.com>
2017-03-01 12:12:41 +0100

    Tilføjet shopList

--- src/ShopBean.java
+++ src/ShopBean.java
@@ -1,6 +1,5 @@
 import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
-import java.io.IOException;
 import java.util.ArrayList;
 
 @ManagedBean(name="shopBean")
@@ -9,30 +8,43 @@ import java.util.ArrayList;
 
 public class ShopBean {
     private ArrayList<Item> prodList = null;
+    private ArrayList<Shop> shopList = null;
     private Item item = null;
     private String message;
     private int shopid = 354;
+    private Shop shop;
 
     public ShopBean() {
+
+        setShop(354);
         rebuildProdList();
+        rebuildShopList();
+    }
+
+    ArrayList<Shop> rebuildShopList() {
+        return new CloudService().listShops();
     }
 
     ArrayList<Item> rebuildProdList() {
-        try {
-            prodList = new CloudService().listItems(shopid);
-        } catch (IOException e) {
-            System.out.println("Genopbygning af produktlisten fejlede: " + e);
-            message = e.getMessage();
-        }
-
-        return prodList;
+        return new CloudService().listItems(shopid);
     }
 
     public ArrayList<Item> getProdList() {
         return prodList;
     }
+    public ArrayList<Shop> getShopList() { return shopList; }
     public void setItem(Item i){ item = i; }
     public Item getItem() {
         return item;
+    }
+    public Shop getShop() {
+        return shop;
+    }
+
+    public void setShop(int shopid) {
+
+            for (Shop s : new CloudService().listShops())
+                if (s.getShopID() == shopid)
+                    this.shop = s;
     }
 }
