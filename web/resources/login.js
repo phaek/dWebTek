15f5fa29c9477ac875c55a187493935da15718b2
phaek <drasmus@gmail.com>
2017-03-05 19:14:20 +0100

    logout() tilføjet og stylet

--- web/resources/login.js
+++ web/resources/login.js
@@ -1,5 +1,6 @@
 function login() {
-    if ((document.getElementById("usernamefield").value != "") && (document.getElementById("passwordfield").value != "")) {
+    console.log("Login er triggered");
+    if ((document.getElementById("usernamefield").value != null) && (document.getElementById("passwordfield").value != null)) {
         sendRequest("POST", "rest/shop/login?username=" + document.getElementById("usernamefield").value + "&password=" + document.getElementById("passwordfield").value, null, function (data) {
             window.alert(data);
         });
@@ -7,7 +8,22 @@ function login() {
         document.getElementById("login").style.display="none";
         document.getElementById("usernamefield").style.display="none";
         document.getElementById("passwordfield").style.display="none";
+
+        document.getElementById("logout").style.display="block";
     }
     else
         window.alert("Udfyld begge felter for at logge ind...");
+}
+
+
+function logout() {
+    sendRequest("POST", "rest/shop/logout", null, function(data) {
+        window.alert(data);
+    });
+
+    document.getElementById("login").style.display="block";
+    document.getElementById("usernamefield").style.display="block";
+    document.getElementById("passwordfield").style.display="block";
+
+    document.getElementById("logout").style.display="none";
 }
