7c22cc88337a4e30ede6860c18fe521fb5b1108b
phaek <drasmus@gmail.com>
2017-03-05 18:42:37 +0100

    Udvidet login via JS så den nu tager input fields. Mangler styling!

--- web/resources/login.js
+++ web/resources/login.js
@@ -1,5 +1,13 @@
-function login(uname, pword) {
-    sendRequest("POST", "rest/shop/login?username=" + uname + "&password=" + pword, null, function(data) {
-        window.alert(data);
-    });
+function login() {
+    if ((document.getElementById("usernamefield").value != "") && (document.getElementById("passwordfield").value != "")) {
+        sendRequest("POST", "rest/shop/login?username=" + document.getElementById("usernamefield").value + "&password=" + document.getElementById("passwordfield").value, null, function (data) {
+            window.alert(data);
+        });
+
+        document.getElementById("login").style.display="none";
+        document.getElementById("usernamefield").style.display="none";
+        document.getElementById("passwordfield").style.display="none";
+    }
+    else
+        window.alert("Udfyld begge felter for at logge ind...");
 }
