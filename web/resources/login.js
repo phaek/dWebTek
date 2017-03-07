eec2418a034ab60c7bcca6c93239f5e1296c25dd
phaek <drasmus@gmail.com>
2017-03-07 10:07:01 +0100

    updateBasket() køres på login()

--- web/resources/login.js
+++ web/resources/login.js
@@ -2,17 +2,16 @@ function login() {
     console.log("Login er triggered");
     if ((document.getElementById("usernamefield").value != null) && (document.getElementById("passwordfield").value != null)) {
         sendRequest("POST", "rest/shop/login?username=" + document.getElementById("usernamefield").value + "&password=" + document.getElementById("passwordfield").value, null, function (data) {
+
+            updateBasket();
+
+            document.getElementById("login").style.display="none";
+            document.getElementById("usernamefield").style.display="none";
+            document.getElementById("passwordfield").style.display="none";
+
+            document.getElementById("logout").style.display="block";
             window.alert(data);
         });
-
-
-        document.getElementById("login").style.display="none";
-        document.getElementById("usernamefield").style.display="none";
-        document.getElementById("passwordfield").style.display="none";
-
-        document.getElementById("logout").style.display="block";
-
-
     }
     else
         window.alert("Udfyld begge felter for at logge ind...");
@@ -23,6 +22,9 @@ function logout() {
     sendRequest("POST", "rest/shop/logout", null, function(data) {
         window.alert(data);
     });
+
+    document.getElementById("produkter").innerHTML = "Indkøbskurv kræver login";
+    document.getElementById("total").innerHTML = "Total <strong>0</strong> kr";
 
     document.getElementById("login").style.display="block";
     document.getElementById("usernamefield").style.display="block";
