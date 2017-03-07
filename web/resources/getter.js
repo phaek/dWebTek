8ee57bc6ec1a23b8bd551c9d9c5d9de03bd4756b
phaek <drasmus@gmail.com>
2017-03-07 20:41:58 +0100

    Doxxed

--- web/resources/getter.js
+++ web/resources/getter.js
@@ -4,9 +4,17 @@ if (!XMLHttpRequest)
 else
     http = new XMLHttpRequest();
 
+/**
+ * Function for handling server requests
+ * @param httpMethod GET, POST, PUT, DELETE, etc
+ * @param url Request URL
+ * @param body Request body
+ * @param responseHandler How to handle the callback data, mostly used with function(data) { stuff }
+ */
 function sendRequest(httpMethod, url, body, responseHandler) {
     http.open(httpMethod, url);
     http.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    http.setRequestHeader("Content-Encoding", "UTF-8");
     http.onreadystatechange = function () {
         if (http.readyState == 4 && http.status == 200) {
             responseHandler(http.responseText);
